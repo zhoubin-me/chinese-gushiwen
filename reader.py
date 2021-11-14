9eaf7a36f9ca6b506b1664fee7abad623969a385
Bin Zhou <zhoubin.me@gmail.com>
2021-11-14 13:53:04 +0800

    .

--- reader.py
+++ reader.py
@@ -1,40 +1,46 @@
 import glob
 import json
 
-with open('guwen/guwen0-1000.json', encoding='utf-8') as f:
-    lines = f.readlines()
-    data = []
-    for line in lines:
-        poem = json.loads(line)
-        content = poem['content']
-        author = poem['writer']
-        title = poem['title']
-        if '·' in title:
-            titles = title.split('·')
-            if titles[1].strip()[0] == '其':
-                title = titles[0].strip()
-            else:
-                title = titles[1].strip()
-        elif '/' in title:
-            title = title.split('/')[0].strip()
-        else:
-            pass
+fs = glob.glob('guwen/*.json')
+data = []
+for fname in fs:
+    with open('guwen/guwen0-1000.json', encoding='utf-8') as f:
+        lines = f.readlines()
+        for line in lines:
+            poem = json.loads(line)
+            content = poem['content']
+            author = poem['writer']
+            title = poem['title']
+            dynasty = poem['dynasty']
 
-        content = content.replace('\n', '').replace(' ', '').replace('\t', '').replace('\r', '')
-        content = content.replace('；', '|').replace('、', '|').replace('，', '|').replace('。', '|').replace('？', '|')
-        if content[-1] == '|':
-            content = content[:-1]
-        if len(content.split('|')) == 4:
-            idata = {'content': content, 'author': author, 'title': title}
-            print(title)
-            data.append(idata)
+            if '一作' in content:
+                continue
+            if '节选' in title:
+                continue
+            if not ('唐代' in dynasty or '宋代' in dynasty):
+                continue
+
+            if '·' in title:
+                titles = title.split('·')
+                if titles[1].strip()[0] == '其':
+                    title = titles[0].strip()
+                else:
+                    title = titles[1].strip()
+            elif '/' in title:
+                title = title.split('/')[0].strip()
+            else:
+                pass
+
+            content = content.replace('\n', '').replace(' ', '').replace('\t', '').replace('\r', '')
+            content = content.replace('；', '|').replace('、', '|').replace('，', '|').replace('！', '|').replace('。', '|').replace('？', '|')
+
+            if content[-1] == '|':
+                content = content[:-1]
+            if len(content.split('|')) <= 8:
+                idata = {'content': content, 'author': author, 'title': title, 'dynasty': dynasty}
+                print(title, dynasty)
+                data.append(idata)
 
 with open('jueju.json', 'w') as f:
     json.dump(data, f)
 print(len(data))
-
-with open('jueju.json', 'r') as f:
-    x = f.read()
-
-with open('jueju2.json', 'w') as f:
-    f.write("jueju2=" + x)
